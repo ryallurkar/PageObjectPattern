bcfe8ba1b6ccbb1ade8ca7ea48bc43f8ef065fc4
Yallurkar, Raju <raju90y@outlook.com>
2019-02-20 16:33:57 +0530

    Added new page objects

--- src/test/java/pageObjects/initializePageObjects/PageFactoryInitializer.java
+++ src/test/java/pageObjects/initializePageObjects/PageFactoryInitializer.java
@@ -3,41 +3,21 @@ package pageObjects.initializePageObjects;
 import org.openqa.selenium.support.PageFactory;
 
 import controllers.BaseMethod;
-import pageObjects.pages.CartPageObjects;
-import pageObjects.pages.CheckoutPageObjects;
-import pageObjects.pages.HomePageObjects;
-import pageObjects.pages.LoginPageObjects;
-import pageObjects.pages.ProductPageObjects;
-import pageObjects.pages.RegistrationPageObjects;
-import pageObjects.pages.SearchPageObjects;
+import pageObjects.pages.AccountPageObjects;
+import pageObjects.pages.DashboardPageObjects;
+import pageObjects.pages.SignUpPageObjects;
 
 public class PageFactoryInitializer extends BaseMethod {
 	
-	public HomePageObjects homePage() {
-		return PageFactory.initElements(getWebDriver(), HomePageObjects.class);
+	public AccountPageObjects accountPage() {
+		return PageFactory.initElements(getWebDriver(), AccountPageObjects.class);
 	}
 	
-	public SearchPageObjects searchPage() {
-		return PageFactory.initElements(getWebDriver(), SearchPageObjects.class);
+	public SignUpPageObjects signUpPage() {
+		return PageFactory.initElements(getWebDriver(), SignUpPageObjects.class);
 	}
 	
-	public ProductPageObjects productsPage() {
-		return PageFactory.initElements(getWebDriver(), ProductPageObjects.class);
-	}
-	
-	public CartPageObjects cartPage() {
-		return PageFactory.initElements(getWebDriver(), CartPageObjects.class);
-	}
-
-	public RegistrationPageObjects registrationPage() {
-		return PageFactory.initElements(getWebDriver(), RegistrationPageObjects.class);
-	}
-	
-	public LoginPageObjects loginPage() {
-		return PageFactory.initElements(getWebDriver(), LoginPageObjects.class);
-	}
-	
-	public CheckoutPageObjects checkOutPage() {
-		return PageFactory.initElements(getWebDriver(), CheckoutPageObjects.class);
+	public DashboardPageObjects dashboardPage() {
+		return PageFactory.initElements(getWebDriver(), DashboardPageObjects.class);
 	}
 }
